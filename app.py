3a60beda8979ccbb810f83532caa9c2a980f331f
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-08-06 23:42:08 -0700

    file display uploads

--- app.py
+++ app.py
@@ -1,59 +1,89 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS, cross_origin
-from pymongo import MongoClient, errors
-from dotenv import load_dotenv
+from flask_cors import CORS
 import os
-import certifi
 
 app = Flask(__name__)
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-load_dotenv()
+UPLOAD_FOLDER = 'uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-app = Flask(__name__)
+DISPLAY_FOLDER = 'display'
+os.makedirs(DISPLAY_FOLDER, exist_ok=True)
+app.config['DISPLAY_FOLDER'] = DISPLAY_FOLDER
 
-client = MongoClient(os.getenv("MONGODB_URI"), tlsCAFile=certifi.where())
-db = client['commandDB']
-collection = db['commands']
-
-@app.route('/posttimes', methods=['POST'])
-def post_data():
+@app.route('/files', methods=['POST'])
+def post_files():
     try:
-        data = request.get_json()
-        if 'responsetime' not in data:
-            return jsonify({"status": "error", "message": "Missing 'timestamp' in request"}), 400
-        print("Received data:", data)
-        collection.insert_one(data)
-        return jsonify({"status": "success"}), 200
-    except errors.PyMongoError as e:
+        files = request.files
+        saved_files = {}
+
+        for file_key in files:
+            file = files[file_key]
+            if file:
+                file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                file.save(file_path)
+                saved_files[file_key] = file_path
+
+        response_data = {
+            "status": "success",
+            "saved_files": saved_files
+        }
+        return jsonify(response_data), 200
+    except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
     
-@app.route('/gettimes', methods=['GET'])
-@cross_origin()
-def get_data():
+@app.route('/files', methods=['GET'])
+def get_files():
     try:
-        data = collection.find()
-        result = [{'command': data['command'],
-                   'commandfile': data['commandfile'],
-                   'commandtime': data['commandtime'],
-                   'response': data['response'],
-                   'responsefile': data['responsefile'],
-                   'responsetime': data['responsetime'],
-                   } for data in data]
-        return jsonify(result), 200
-    except errors.PyMongoError as e:
+        files = os.listdir(app.config['UPLOAD_FOLDER'])
+        file_info = [{"filename": file, "filepath": os.path.join(app.config['UPLOAD_FOLDER'], file)} for file in files]
+
+        return jsonify(file_info), 200
+    except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+    
+@app.route('/display', methods=['POST'])
+def post_display_files():
+    try:
+        files = request.files
+        saved_files = {}
+
+        for file_key in files:
+            file = files[file_key]
+            if file:
+                file_path = os.path.join(app.config['DISPLAY_FOLDER'], file.filename)
+                file.save(file_path)
+                saved_files[file_key] = file_path
+
+        response_data = {
+            "status": "success",
+            "saved_files": saved_files
+        }
+        return jsonify(response_data), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
 
 @app.route('/')
 def index():
     try:
-        data = collection.find()
-        result = [{key: value for key, value in item.items() if key != '_id'} for item in data]
-        return jsonify(result), 200
-    except errors.PyMongoError as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        upload_files = os.listdir(app.config['UPLOAD_FOLDER'])
+        upload_file_info = [{"filename": file, "filepath": os.path.join(app.config['UPLOAD_FOLDER'], file)} for file in upload_files]
 
+        display_files = os.listdir(app.config['DISPLAY_FOLDER'])
+        display_file_info = [{"filename": file, "filepath": os.path.join(app.config['DISPLAY_FOLDER'], file)} for file in display_files]
+
+        all_files_info = {
+            "uploaded_files": upload_file_info,
+            "display_files": display_file_info
+        }
+
+        return jsonify(all_files_info), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

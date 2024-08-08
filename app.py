2a1489d69d74842bd7c37c5157655549da1786be
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-08-07 22:31:45 -0700

    connect socket

--- app.py
+++ app.py
@@ -118,4 +118,5 @@ def index():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    socketio.run(app)
+    # app.run(host='0.0.0.0', port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000)

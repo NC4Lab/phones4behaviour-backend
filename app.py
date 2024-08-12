84f15bbfb4d85d8dafd0c9d357991bde464aebaa
gracetxgao <101677420+gracetxgao@users.noreply.github.com>
2024-08-11 20:17:03 -0700

    redid rest api requests and added logs

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
+import json
 import os
 import shutil
 import mimetypes
@@ -20,26 +21,52 @@ UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-DISPLAY_FOLDER = 'display'
+UPLOAD_JSON = 'uploads/uploads.json'
+app.config['UPLOAD_JSON'] = UPLOAD_JSON
+
+DISPLAY_FOLDER = 'uploads/display'
 os.makedirs(DISPLAY_FOLDER, exist_ok=True)
 app.config['DISPLAY_FOLDER'] = DISPLAY_FOLDER
 
-@app.route('/files', methods=['POST'])
+DISPLAY_JSON = 'uploads/display/display.json'
+app.config['DISPLAY_JSON'] = DISPLAY_JSON
+
+LOG_JSON = 'logs.json'
+app.config['LOG_JSON'] = LOG_JSON
+
+@app.route('/uploads', methods=['POST'])
 def post_files():
     try:
         files = request.files
+        file_type = request.form.get('fileType')
+        upload_time = request.form.get('uploadTime')
         saved_files = {}
+
+
+        with open(UPLOAD_JSON, 'r') as f:
+            uploads_json = json.load(f)
+
 
         for file_key in files:
             file = files[file_key]
             if file:
                 file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
                 file.save(file_path)
-                filetype = file.mimetype
                 saved_files[file_key] = {
-                        "filepath": file_path,
-                        "filetype": filetype 
-                    }
+                    "file_path": file_path,
+                    "file_type": file_type,
+                    "upload_time": upload_time,
+                }
+
+                uploads_json.append({
+                    "file_name": file.filename,
+                    "file_path": file_path,
+                    "file_type": file_type,
+                    "upload_time": upload_time
+                })
+
+        with open(UPLOAD_JSON, 'w') as f:
+            json.dump(uploads_json, f)
 
         response_data = {
             "status": "success",
@@ -49,31 +76,43 @@ def post_files():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
     
-@app.route('/files', methods=['GET'])
+@app.route('/uploads', methods=['GET'])
 def get_files():
     try:
+        with open(UPLOAD_JSON, 'r') as f:
+            uploads_json = json.load(f)
+            
         files = os.listdir(app.config['UPLOAD_FOLDER'])
-        file_info = []
+        response = []
 
         for file in files:
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file)
-            filetype = mimetypes.guess_type(file_path)
-            file_info.append({
-                "filename": file,
-                "filepath": file_path,
-                "filetype": filetype
-            })
+            for entry in uploads_json:
+                if entry['file_name'] == file:
+                    file_info = {
+                        'file_name': entry.get('fileName', 'unknown'),
+                        'file_path': entry.get('file_path', 'unknown'),
+                        'file_type': entry.get('file_type', 'unknown'),
+                        'upload_time': entry.get('upload_time', 'unknown')
+                    }
+                    response.append(file_info)
+                    break
 
-        return jsonify(file_info), 200
+        return jsonify(response), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-
-@app.route('/display', methods=['POST'])
+@app.route('/uploads/<filename>', methods=['GET'])
+def serve_upload_file(filename):
+    try:
+        return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+    
+@app.route('/uploads/display', methods=['POST'])
 def post_display_files():
     try:
-        for filename in os.listdir(app.config['DISPLAY_FOLDER']):
-            file_path = os.path.join(app.config['DISPLAY_FOLDER'], filename)
+        for file_name in os.listdir(app.config['DISPLAY_FOLDER']):
+            file_path = os.path.join(app.config['DISPLAY_FOLDER'], file_name)
             if os.path.isfile(file_path):
                 os.unlink(file_path)
 
@@ -81,15 +120,18 @@ def post_display_files():
         selected_files = data.get('files', [])
         saved_files = {}
 
-        for filename in selected_files:
-            src_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            dest_path = os.path.join(app.config['DISPLAY_FOLDER'], filename)
+        for file in selected_files:
+            file_name = file.get('fileName')
+            display_time = file.get('displayTime')
+            src_path = os.path.join(app.config['UPLOAD_FOLDER'], file_name)
+            dest_path = os.path.join(app.config['DISPLAY_FOLDER'], file_name)
             if os.path.exists(src_path):
                 shutil.copy(src_path, dest_path)
-                filetype, _ = mimetypes.guess_type(filename)
-                saved_files[filename] = {
-                    "filepath": dest_path,
-                    "filetype": filetype
+                file_type, _ = mimetypes.guess_type(file_name)
+                saved_files[file_name] = {
+                    "file_path": dest_path,
+                    "file_type": file_type,
+                    "display_time": display_time
                 }
 
         socketio.emit('new_file', {'files': selected_files})
@@ -102,7 +144,7 @@ def post_display_files():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
-@app.route('/display', methods=['GET'])
+@app.route('/uploads/display', methods=['GET'])
 def get_display_files():
     try:
         files = os.listdir(app.config['DISPLAY_FOLDER'])
@@ -110,40 +152,77 @@ def get_display_files():
 
         for file in files:
             file_path = os.path.join(app.config['DISPLAY_FOLDER'], file)
-            mime_type, _ = mimetypes.guess_type(file) 
+            file_type, _ = mimetypes.guess_type(file) 
             file_info.append({
-                "filename": file,
-                "filepath": file_path,
-                "filetype": mime_type
+                "file_name": file,
+                "file_path": file_path,
+                "file_type": file_type
             })
         return jsonify(file_info), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
     
-@app.route('/display/<filename>', methods=['GET'])
+@app.route('/uploads/display/<filename>', methods=['GET'])
 def serve_display_file(filename):
     try:
         return send_from_directory(app.config['DISPLAY_FOLDER'], filename)
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
-    
-@app.route('/')
-def index():
+
+@app.route('/logs', methods=['POST'])
+def post_logs():
     try:
-        upload_files = os.listdir(app.config['UPLOAD_FOLDER'])
-        upload_file_info = [{"filename": file, "filepath": os.path.join(app.config['UPLOAD_FOLDER'], file)} for file in upload_files]
+        with open(LOG_JSON, 'r') as f:
+            logs_json = json.load(f)
 
-        display_files = os.listdir(app.config['DISPLAY_FOLDER'])
-        display_file_info = [{"filename": file, "filepath": os.path.join(app.config['DISPLAY_FOLDER'], file)} for file in display_files]
+        new_log = request.json
+        if new_log:
+            logs_json.append(new_log)
 
-        all_files_info = {
-            "uploaded_files": upload_file_info,
-            "display_files": display_file_info
+        temp_file = LOG_JSON + '.tmp'
+        with open(temp_file, 'w') as f:
+            json.dump(logs_json, f)
+        os.replace(temp_file, LOG_JSON)
+
+        with open(LOG_JSON, 'w') as f:
+            json.dump(logs_json, f)
+
+        response_data = {
+            "status": "success",
+            "saved_files": new_log
         }
-
-        return jsonify(all_files_info), 200
+        return jsonify(response_data), 200
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
+    
+@app.route('/logs', methods=['GET'])
+def get_logs():
+    try:
+        with open(LOG_JSON, 'r') as f:
+            logs_json = json.load(f)
+
+        return jsonify(logs_json), 200
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
+# @app.route('/')
+# def index():
+#     try:
+#         upload_files = os.listdir(app.config['UPLOAD_FOLDER'])
+#         upload_file_info = [{"filename": file, "filepath": os.path.join(app.config['UPLOAD_FOLDER'], file)} for file in upload_files]
+
+#         display_files = os.listdir(app.config['DISPLAY_FOLDER'])
+#         display_file_info = [{"filename": file, "filepath": os.path.join(app.config['DISPLAY_FOLDER'], file)} for file in display_files]
+
+#         all_files_info = {
+#             "uploaded_files": upload_file_info,
+#             "display_files": display_file_info
+#         }
+
+#         return jsonify(all_files_info), 200
+#     except Exception as e:
+#         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=5000)
